3bc2569c0eedc8ddcb450812748081310d8372f0
Mark Guerra <markgx@gmail.com>
2013-12-21 01:03:51 -0800

    initial commit

---
+++ closure.go
@@ -0,0 +1,23 @@
+package closure
+
+import (
+	"os/exec"
+)
+
+func Compile(files *[]string, outputFilePath string) {
+	args := make([]string, len(*files)*2+2)
+
+	for i, file := range *files {
+		args[i*2] = "--js"
+		args[i*2+1] = file
+	}
+
+	args[len(args)-2] = "--js_output_file"
+	args[len(args)-1] = outputFilePath
+
+	cmd := exec.Command("closure-compiler", args...)
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
